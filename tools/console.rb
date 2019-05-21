d4a7664939cb1dfbf2b249c519524a85f9afdaeb
Bluesbaka <badunnells@gmail.com>
2019-05-20 18:32:35 -0700

    Done.

--- tools/console.rb
+++ tools/console.rb
@@ -4,4 +4,14 @@ def reload
   load 'config/environment.rb'
 end
 
+l1 = Listing.new("Seattle")
+l2 = Listing.new("San Fransisco")
+
+g1 = Guest.new('bill')
+g2 = Guest.new('carmen')
+
+t1 = Trip.new(g1, l2)
+t2 = Trip.new(g2, l1)
+t3 = Trip.new(g2, l2)
+
 Pry.start
