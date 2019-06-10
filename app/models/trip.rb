9c317a687529caa1e854d4816dc6b587d18d5f6d
github username <github email address>
2019-06-10 15:53:51 -0700

    airbnb done

---
+++ app/models/trip.rb
@@ -0,0 +1,27 @@
+require 'pry'
+
+class Trip
+attr_reader :listing, :guest
+@@all = []
+
+  def initialize(listing, guest)
+    @listing = listing
+    @guest = guest
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def find_listing
+    self.listing
+  end
+
+  def find_guest
+    self.guest
+  end
+
+
+
+end
--- tools/console.rb
+++ tools/console.rb
@@ -1,7 +1,28 @@
 require_relative '../config/environment.rb'
+require_relative '../app/models/guest.rb'
+require_relative '../app/models/listing.rb'
+require_relative '../app/models/trip.rb'
 
-def reload
-  load 'config/environment.rb'
-end
+require 'pry'
+
+# def reload
+#   load 'config/environment.rb'
+# end
+
+g1 = Guest.new("Aliya")
+g2 = Guest.new("Philip")
+g3 = Guest.new("Rohit")
+
+l1 = Listing.new("Seattle")
+l2 = Listing.new("Brooklyn")
+l3 = Listing.new("Belize City")
+
+t1 = Trip.new(l2, g1)
+t2 = Trip.new(l2, g3)
+t3 = Trip.new(l3, g2)
+t4 = Trip.new(l1, g2)
+
+
 
 Pry.start
+0
